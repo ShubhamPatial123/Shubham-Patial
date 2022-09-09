2a09afd987761e92e77617d3bdddcfd1f211abdc
Shubham <shubhampatial750@gmail.com>
2022-09-09 14:07:21 +0530

    changes

--- hello/settings.py
+++ hello/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=m6d=p%@5*hs-v5p@w9-kshji__%$xz4k-3lclr7pj736bh@88'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["shubu750.herokuapp.com"]
 
 
 # Application definition
